0bac0a699f7a82f735136bb715ffbaabdebc7330
Chris Clark <chris@chrisclark.uk>
2023-10-15 15:21:26 +0000

    Create default users

--- server/stores.ts
+++ server/stores.ts
@@ -1,3 +1,14 @@
 import { UserRecord } from "./types";
 
-export const userStore: Record<string, UserRecord> = {};
+export const userStore: Record<string, UserRecord> = {
+  "user1": {
+    id: "user1",
+    createdAt: new Date('2023-10-11T11:50:40.000Z'),
+  },
+  "admin1": {
+    id: "admin1",
+    createdAt: new Date('2022-10-12T14:50:40.000Z'),
+    isAdministrator: true,
+  }
+};
+
