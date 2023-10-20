855c42b901a5db9c3443b7a2f447624c489e9a8b
Chris Clark <chris@chrisclark.uk>
2023-10-20 16:00:34 +0000

    Apply prettier formatting

--- server/stores.ts
+++ server/stores.ts
@@ -1,14 +1,13 @@
 import { UserRecord } from "./types";
 
 export const userStore: Record<string, UserRecord> = {
-  "user1": {
+  user1: {
     id: "user1",
-    createdAt: new Date('2023-10-11T11:50:40.000Z'),
+    createdAt: new Date("2023-10-11T11:50:40.000Z"),
   },
-  "admin1": {
+  admin1: {
     id: "admin1",
-    createdAt: new Date('2022-10-12T14:50:40.000Z'),
+    createdAt: new Date("2022-10-12T14:50:40.000Z"),
     isAdministrator: true,
-  }
+  },
 };
-
