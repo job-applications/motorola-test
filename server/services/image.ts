aa2c2fe388a5a17bc5c342bba5482d64a1fef978
Chris Clark <chris@chrisclark.uk>
2023-10-22 21:46:24 +0000

    Add image processing

--- server/services/image.ts
+++ server/services/image.ts
@@ -7,6 +7,7 @@ import {
   UserRecord,
 } from "../types";
 import { imageStore } from "../stores";
+import { detectFaces } from "./face";
 
 export const addImage = (
   user: UserRecord,
@@ -26,6 +27,11 @@ export const addImage = (
 
   imageStore[imageId] = imageRecord;
 
+  // Introduce an artificial delay
+  setTimeout(() => {
+    runImageProcessing(imageRecord);
+  }, 5000);
+
   return getImageWithoutBuffer(imageRecord);
 };
 
@@ -40,4 +46,23 @@ export const getImageWithoutBuffer = (
 ): ImageRecordWithoutBuffer => {
   const { file, thumb, ...rest } = image;
   return rest;
+};
+
+export const getImage = (imageId: string): ImageRecord => {
+  return imageStore[imageId];
+};
+
+export const updateImage = (imageId: string, image: ImageRecord): void => {
+  imageStore[imageId] = image;
+};
+
+export const runImageProcessing = async (image: ImageRecord) => {
+  console.log(`Processing image ${image.id}`);
+
+  const detections = await detectFaces(image.file.data);
+  updateImage(image.id, {
+    ...image,
+    status: ImageStatus.Completed,
+    faces: detections.length,
+  });
 };
