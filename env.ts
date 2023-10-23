fadbcd855af3affe1d79283f8c76f4f63a7f8679
Chris Clark <chris@chrisclark.uk>
2023-10-23 13:57:34 +0000

    Docker file changes

--- env.ts
+++ env.ts
@@ -3,7 +3,11 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    JWT_TOKEN: z.string(),
+    JWT_TOKEN: z
+      .string()
+      .default(
+        "ed09d0cae9041017037aeff7d327cc402b401140421e1f52b65c0a99415c78fa",
+      ),
     JWT_TOKEN_EXPIRES_IN: z.string().default("30m"),
   },
   client: {},
