de0cf3d5966297315b1cfa14f239a501e2e0a43b
Chris Clark <chris@chrisclark.uk>
2023-10-23 13:18:59 +0000

    Update file path

--- server/services/face.ts
+++ server/services/face.ts
@@ -12,7 +12,7 @@ async function setupFaceApi(): Promise<void> {
     return;
   }
 
-  const MODEL_URL = "/home/workbench/motorola/models";
+  const MODEL_URL = "./models";
   await faceapi.nets.ssdMobilenetv1.loadFromDisk(MODEL_URL);
 }
 
